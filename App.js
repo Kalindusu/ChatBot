7b027128bc6d3bf69b8cc7895c525a9b915f5395
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-18 16:32:34 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, FlatList, TextInput, TouchableOpacity,SafeAreaView } from "react-native";
+import Response from "./components/response";
+import Message from "./components/message";
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+
+
+
+
+
+
+export default function App() {
+	const [inputText, setInputText] = useState("");
+	const [listData, setListData] = useState([]);
+	const SearchInput = () => {
+		setListData((prevList) => [...prevList, inputText]);
+		setInputText("");
+	};
+
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+
+			
+			{/* Header */}
+			<View style={styles.header}>
+				<Image source={require("./assets/icons/robot.png") } style={styles.icon} />
+				<Text style={{ fontSize: 25 , fontWeight: "800", color: "white" }}>ChatBot </Text>
+				
+			</View>
+			{/* <View><ChatAi/></View> */}
+
+			{/* Content */}
+			<FlatList
+				style={{ paddingHorizontal: 16, marginBottom: 80 }}
+				data={listData}
+				renderItem={({ item }) => (
+					<View>
+						<Message message={item} />
+						<Response prompt={item} />
+					</View>
+				)}
+				keyExtractor={(item, index) => index.toString()}
+			/>
+
+			{/* Search-Bar */}
+			<View style={styles.searchBar}>
+				<TextInput placeholder="Ask to PitCrew AI" style={styles.input} value={inputText} onChangeText={(text) => setInputText(text)} selectionColor={"#323232"}></TextInput>
+				<TouchableOpacity onPress={SearchInput}>
+					<Image source={require("./assets/icons/right-arrow.png")} style={styles.icon} />
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		gap: 16,
+		paddingTop: 30,
+		
+	},
+	header: {
+		flexDirection: "row",
+		width: '100%',
+		 height: 70,
+		  marginTop: 10,
+		 
+		  
+		 
+		backgroundColor:'#291D7D',
+		paddingTop:20,
+		paddingLeft:60
+	},
+	icon: {
+		width: 35,
+		height: 35,
+		marginRight:30
+	},
+	searchBar: {
+		backgroundColor: "#ffffff",
+		width: "100%",
+		position: "absolute",
+		bottom: 0,
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		paddingHorizontal: 32,
+		paddingVertical: 16,
+		gap: 8,
+	},
+	input: {
+		backgroundColor: "#fff",
+		width: "100%",
+		fontSize: 16,
+		paddingVertical: 16,
+		paddingHorizontal: 24,
+		borderRadius: 32,
+		borderWidth: 0.1,
+	},
+});
---
+++ README.md
@@ -0,0 +1,51 @@
+
+#  Chat-Bot in React Native
+
+This project contains a basic level chat ui and you can chat with the gemini ai. 
+Ask Everything. I dont know how can you focus the gemini for the one project 
+
+## Installation
+
+Clone the project
+
+```bash
+  git clone https://github.com/ErenElagz/Beginner-Chat-Bot-in-React-Native-Gemini
+```
+
+Go to the project directory
+
+```bash
+  cd Beginner-Chat-Bot-in-React-Native-Gemini
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+Get Your Gemini API Key from this link and change the API_KEY value of components/response.js 
+
+```bash
+https://aistudio.google.com/app/apikey
+```
+
+Start the App
+
+```bash
+  npm start
+```
+
+
+## 🔗 Links
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](www.linkedin.com/in/kalindu-sudaraka-3b24482a3)
+
+
+
+
+
+
+## Support
+
+For support, email to me, my email: erenelagoz@gmail.com or send pull request its up to you
+
